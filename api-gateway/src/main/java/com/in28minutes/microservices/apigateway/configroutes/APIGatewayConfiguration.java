ad950a8c4d1c17cfbebce0d2750b84a1220ad709
lakshay8088 <107973384+lakshay8088@users.noreply.github.com>
2024-05-06 10:45:21 +0530

    Add files via upload

---
+++ api-gateway/src/main/java/com/in28minutes/microservices/apigateway/configroutes/APIGatewayConfiguration.java
@@ -0,0 +1,20 @@
+package com.in28minutes.microservices.apigateway.configroutes;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class APIGatewayConfiguration {
+	
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p.path("/currency-exchange/**").uri("lb://currency-exchange"))
+				.route(p -> p.path("/currency-conversion/**").uri("lb://currency-conversion"))
+				.route(p -> p.path("/currency-conversion-feign/**").uri("lb://currency-conversion"))
+				.build();
+	}
+
+}
